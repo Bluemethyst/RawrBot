09ce4f411b61ed9be701c94d82ffefa6b2c29be1
Bluemethyst <Bluemethyst@users.noreply.github.com>
2024-01-27 16:28:12 +1300

    errors on import

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import nextcord
-import os
-import dotenv
-import colorama
-from nextcord.ext import commands
-from colorama import Fore as col
+
+try:
+    import nextcord
+    import os
+    import dotenv
+    import colorama
+    from nextcord.ext import commands
+    from colorama import Fore as col
+except ImportError:
+    import os
+    os.system("python -m pip install colorama")
+    os.system("python -m pip install nextcord")
+    os.system("python -m pip install python-dotenv")
+    import nextcord
+    import dotenv
+    import colorama
+    from nextcord.ext import commands
+    from colorama import Fore as col
 
 dotenv.load_dotenv()
 colorama.init(autoreset=True)
